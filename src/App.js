b65a33c5e1f0974a3f09fa138413c2f5e847a886
Abhishek Yadav <abhishekyadav@Abhisheks-MacBook-Pro.local>
2023-11-21 12:02:01 +0530

    added material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,98 @@
 import './App.css';
+import { ThemeProvider, createTheme, styled } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#7053FF',
+    },
+    secondary: {
+      main: '#A495FD',
+    },
+    tertiary: {
+      main: '#323232',
+      light: '#4A4A68',
+    },
+    danger: {
+      main: '#E65E69',
+    },
+    white: {
+      main: '#FFFFFF',
+    },
+  },
+  typography: {
+    button: {
+      fontSize: '0.7rem',
+      textTransform: 'capitalize',
+    },
+    fontSize: 14,
+    fontFamily: ['Montserrat', 'Rubik', 'Open Sans'].join(','),
+    extraLarge: {
+      fontFamily: 'Rubik',
+      fontSize: '80px',
+      fontWeight: 700,
+      '@media (max-width:768px)': {
+        fontSize: '24px',
+      },
+    },
+    large: {
+      fontFamily: 'Rubik',
+      fontSize: '48px',
+      fontWeight: 700,
+      '@media (max-width:768px)': {
+        fontSize: '24px',
+      },
+    },
+    h1: {
+      fontFamily: 'Rubik',
+      fontSize: '32px',
+      fontWeight: 700,
+      '@media (max-width:768px)': {
+        fontSize: '24px',
+      },
+    },
+    h2: {
+      fontFamily: 'Montserrat',
+      fontSize: '18px',
+      fontWeight: 600,
+    },
+    body: {
+      fontFamily: 'Rubik',
+      fontSize: '16px',
+      fontWeight: 600,
+    },
+    subtitle: {
+      fontFamily: 'Montserrat',
+      fontSize: '12px',
+      fontWeight: 400,
+    },
+    label: {
+      fontFamily: 'Montserrat',
+      fontSize: '13px',
+      fontWeight: 600,
+      '@media (max-width:1450px)': {
+        fontSize: '11px',
+      },
+    },
+  },
+});
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <AppContainer></AppContainer>
+    </ThemeProvider>
+  );
 }
+
+const AppContainer = styled('div')(({ theme }) => ({
+  padding: '0',
+  margin: '0 auto',
+  overflowX: 'hidden',
+
+  '@media (max-width: 768px)': {
+    padding: '0 16px',
+  },
+}));
 
 export default App;
